0bf22f46daa3048f3f46e40f877a27dc1b8330f8
Trong Dat <trongdat@nexondv.com>
2024-03-12 18:46:07 +0700

    handle multiple inputs, add @types/bun

--- index.ts
+++ index.ts
@@ -1,50 +1,62 @@
 import puppeteer, { Browser } from "puppeteer"
 import * as cheerio from "cheerio"
 
-let browser: Browser
+let browser: Browser | undefined
 
 async function fetchHTML(url: string) {
     if (!browser) {
-        browser = await puppeteer.launch({ headless: true })
+        browser = await puppeteer.launch({
+            headless: true,
+            executablePath: "/usr/bin/google-chrome",
+        })
     }
 
     const page = await browser.newPage()
     await page.goto(url, { waitUntil: "networkidle0" }) // Use domcontentloaded for faster wait
     const html = await page.content()
-    await browser.close()
 
     return html
 }
 
-const scrapeFirstPage = async (searchInput: string) => {
+const scrapeFirstPage = async (searchInputs: string[]) => {
     try {
-        const html = await fetchHTML(
-            `https://www.google.com/search?q=${searchInput.split(" ").join("+")}`
-        )
-        const $ = cheerio.load(html)
-        let totalLinks: number
-        let totalAdWord: number
-        let totalResults: string
+        const promises = searchInputs.map(async (input) => {
+            const url = `https://www.google.com/search?q=${input.split(" ").join("+")}`
+            const html = await fetchHTML(url)
+            const $ = cheerio.load(html)
+            let totalLinks: number
+            let totalAdWord: number
+            let totalResults: string
 
-        const totalResultsTag = $("#result-stats")
-        totalResults = $(totalResultsTag).text().trim()
+            const totalResultsTag = $("#result-stats")
+            totalResults = $(totalResultsTag).text().trim()
 
-        const adWordTags = $("#tvcap")
-        totalAdWord = adWordTags.length
+            const adWordTags = $("#tvcap")
+            totalAdWord = adWordTags.length
 
-        const aTags = $("a")
-        totalLinks = aTags.length
+            const aTags = $("a")
+            totalLinks = aTags.length
 
-        console.log({
-            totalAdWord,
-            totalLinks,
-            totalResults,
-            html,
-            // html: html.length,
+            return {
+                totalAdWord,
+                totalLinks,
+                totalResults,
+                // html,
+                html: html.length,
+            }
         })
+
+        const scrapedData = await Promise.all(promises)
+        console.log(scrapedData)
     } catch (error) {
-        console.error("Error")
+        console.error(error)
+    } finally {
+        if (browser) {
+            await browser.close()
+            browser = undefined
+            return
+        }
     }
 }
 
-await scrapeFirstPage("scrape google")
+await scrapeFirstPage(["scrape google", "react", "vue"])
