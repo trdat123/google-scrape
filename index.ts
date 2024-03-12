2871504edf238c0da6fa31f952373fff53a5f159
Trong Dat <datngt1111@gmail.com>
2024-03-12 17:19:55 +0700

    refactor main func

--- index.ts
+++ index.ts
@@ -3,31 +3,31 @@ import * as cheerio from "cheerio"
 
 let browser: Browser
 
-async function fetchHTML(url) {
+async function fetchHTML(url: string) {
     if (!browser) {
         browser = await puppeteer.launch({ headless: true })
     }
+
     const page = await browser.newPage()
     await page.goto(url, { waitUntil: "networkidle0" }) // Use domcontentloaded for faster wait
-
     const html = await page.content()
-    browser.close()
+    await browser.close()
+
     return html
 }
 
-const scrapeFirstPage = async () => {
+const scrapeFirstPage = async (searchInput: string) => {
     try {
         const html = await fetchHTML(
-            "https://www.google.com/search?q=scrapegoogle"
+            `https://www.google.com/search?q=${searchInput.split(" ").join("+")}`
         )
         const $ = cheerio.load(html)
-        const h3s: string[] = []
         let totalLinks: number
         let totalAdWord: number
         let totalResults: string
 
         const totalResultsTag = $("#result-stats")
-        totalResults = $(totalResultsTag).text()
+        totalResults = $(totalResultsTag).text().trim()
 
         const adWordTags = $("#tvcap")
         totalAdWord = adWordTags.length
@@ -35,21 +35,16 @@ const scrapeFirstPage = async () => {
         const aTags = $("a")
         totalLinks = aTags.length
 
-        const h3Tags = $("h3")
-        h3Tags.each((i, h3Tag) => {
-            const text = $(h3Tag).text().trim()
-            h3s.push(text)
-        })
-
         console.log({
             totalAdWord,
             totalLinks,
             totalResults,
             html,
+            // html: html.length,
         })
     } catch (error) {
         console.error("Error")
     }
 }
 
-await scrapeFirstPage()
+await scrapeFirstPage("scrape google")
